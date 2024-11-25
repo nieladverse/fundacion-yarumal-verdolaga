7f707d4c52c8bfed041644d93617592a2120a5ac
NieladVerse <nieladverse@gmail.com>
2024-11-25 13:49:26 -0500

    [DEV] - fixing route to projects

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Layaut from "./components/Layaut.jsx";
 import Nosotros from "./pages/Nosotros.jsx";
-import Proyectos from "./pages/Nosotros.jsx";
+import Proyectos from "./pages/Proyectos.jsx";
 import ThemeProvider from "./context/ThemeContext.jsx";
 
 export default function App() {
